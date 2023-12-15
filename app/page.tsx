99c11ff4f3879bba6c61bdd95144a53808d4ca8d
aaron <aaronakamm@gmail.com>
2023-12-15 12:01:08 -0800

    Basic index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-// import Image from 'next/image'
+import Search from "./components/Search";
 
 export default function Home() {
-  return <div>Welcome to Weather App</div>
+  return <div className='flex justify-center items-center flex-col'>
+    <h1 className='font-bold'>Welcome to Weather App!</h1>
+    <div>
+      <p>Enter your zip code to retrieve the weather:</p>
+      <Search/>
+    </div>
+  </div>
 }
